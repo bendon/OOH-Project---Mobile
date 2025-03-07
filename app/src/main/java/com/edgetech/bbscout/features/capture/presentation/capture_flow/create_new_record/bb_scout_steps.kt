d6820d19013cf7bc591032f1027bdbb3eb47385c
Emery <bemeryb@gmail.com>
2025-03-07 16:41:34 +0300

    added the BBScoutStepper composable and related logic.

---
+++ app/src/main/java/com/edgetech/bbscout/features/capture/presentation/capture_flow/create_new_record/bb_scout_steps.kt
@@ -0,0 +1,45 @@
+package com.edgetech.bbscout.features.capture.presentation.capture_flow.create_new_record
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+
+@Composable
+fun BBScoutStepper(
+    numberOfSteps: Int,
+    currentStep: Int,
+    modifier: Modifier = Modifier
+){
+    Surface(
+        modifier = modifier.height(4.dp),
+        color = MaterialTheme.colorScheme.surface,
+        shape = MaterialTheme.shapes.small
+    ) {
+        Row {
+            for (i in 1..numberOfSteps){
+                Stepper(isComplete = i <= currentStep, modifier = Modifier.weight(1f))
+            }
+        }
+    }
+}
+
+@Composable
+fun Stepper(
+isComplete: Boolean,
+modifier: Modifier = Modifier
+){
+    Surface(
+        modifier = modifier.fillMaxHeight(),
+        color = if(isComplete) MaterialTheme.colorScheme.surface else Color.Transparent,
+        shape = MaterialTheme.shapes.small
+    ) {
+
+    }
+}
